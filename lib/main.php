eee9b3574830b1b748e73389b2719f9afd81aa99
Sobenkov <Sobenkov95@yandex.ru>
2025-08-12 12:45:33 +0300

    DEV TR первичная структура lib/main.php

--- lib/main.php
+++ lib/main.php
@@ -1,3 +1,27 @@
-<?
+<?php
+namespace tr\BotGuard;
 
-?>
+use Bitrix\Main\Config\Option;
+use Bitrix\Main\IO\File;
+use Bitrix\Main\IO\Directory;
+
+class Main
+{
+    protected $moduleId = 'tr.botguard';
+
+    public function checkVisitor()
+    {
+
+    }
+
+    protected function log($file, $message)
+    {
+        file_put_contents($file, '[' . date('Y-m-d H:i:s') . '] ' . $message . "\n", FILE_APPEND);
+    }
+
+    protected function denyAccess()
+    {
+        header('HTTP/1.1 403 Forbidden');
+        exit();
+    }
+}
